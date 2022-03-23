5a0e56b237c9957a6095210bbf37a62456b6ad6d
unknown <Saagar>
2022-03-23 11:28:31 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Sunshine Specialist Supports</title>
+	<style>
+		body {
+			padding: 0px;
+			margin: 0px;
+		}
+
+		.outer-container {
+			margin-top: 50px;
+		}
+
+		.logo {
+			width: 20%;
+			margin: auto;
+		}
+
+		.logo-container {
+			text-align: center;
+		}
+	</style>
+</head>
+<body>
+<div class="outer-container">
+	<div class="logo-container">
+		<img src="images/live-logo.png" class="logo">
+	</div>
+</div>
+</body>
+</html>
